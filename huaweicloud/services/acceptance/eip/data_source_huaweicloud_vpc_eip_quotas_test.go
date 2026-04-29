6297c35672da1b3fbf1d62c91351671a7eeaa06b
wangyan <dursinlabiaus@hotmail.com>
2026-04-29 15:05:28 +0800

    feat(eip): add vpc eip quotas datasource

---
+++ huaweicloud/services/acceptance/eip/data_source_huaweicloud_vpc_eip_quotas_test.go
@@ -0,0 +1,36 @@
+package eip
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
+)
+
+func TestAccDataSourceVpcEipQuotas_basic(t *testing.T) {
+	dataSource := "data.huaweicloud_vpc_eip_quotas.test"
+	dc := acceptance.InitDataSourceCheck(dataSource)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acceptance.TestAccPreCheck(t)
+		},
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceVpcEipQuotas_basic,
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttrSet(dataSource, "quotas.0.type"),
+					resource.TestCheckResourceAttrSet(dataSource, "quotas.0.used"),
+					resource.TestCheckResourceAttrSet(dataSource, "quotas.0.quota"),
+				),
+			},
+		},
+	})
+}
+
+const testAccDataSourceVpcEipQuotas_basic = `
+data "huaweicloud_vpc_eip_quotas" "test" {}
+`
